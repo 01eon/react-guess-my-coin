e083349a2a96cb5287d4ff8cbd0de27137d0b5a5
Gabriel Austria <brhymed01@gmail.com>
2025-05-30 14:33:09 +0800

    Added: Modal Content's Media Query

--- src/components/modal/modal.style.jsx
+++ src/components/modal/modal.style.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { media } from "../media-query/media-query.styled";
 
 const ModalContainer = styled.div`
   width: 100%;
@@ -11,6 +12,21 @@ const ModalContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  ${media.standardPhone`
+    align-items: flex-start;
+    padding-top: 0.3rem;
+  `}
+
+  ${media.hdPhone`
+    padding-top: 1.6rem;
+  `}
+
+  ${media.fhdPhone`
+    padding-top: 0;
+    align-items: center;
+  `}
+
 `;
 
 export const ModeContainer = styled(ModalContainer)`
@@ -22,7 +38,7 @@ export const DiffContainer = styled(ModalContainer)`
 `;
 
 export const ModalContent = styled.div`
-  background-color: white;
+  background-color: #fff;
   width: 50rem;
   display: flex;
   flex-direction: column;
@@ -31,21 +47,48 @@ export const ModalContent = styled.div`
   align-items: center;
   padding: 0.5rem;
   border-radius: 1rem;
+
+  ${media.standardPhone`
+    margin: 0.5rem 1rem;
+  `}
+
+  ${media.hdPhone`
+    margin: 0.5rem 5.5rem;
+  `}
 `;
 
 export const ModalTitle = styled.h2`
   text-transform: capitalize;
+  text-align: center;
   font-size: 2.5rem;
   font-weight: 800;
   background: linear-gradient(135deg, #667eea, #764ba2);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   letter-spacing: -0.02em;
+
+  ${media.standardPhone`
+    font-size: 2rem;
+    `}
+
+  ${media.hdPhone`
+      font-size: 2.5rem;
+      `}
 `;
 
 export const ChoiceContainer = styled.div`
   display: flex;
   gap: 0.5rem;
+
+  ${media.standardPhone`
+    flex-direction: column;
+    gap: 0;  
+  `}
+
+  ${media.fhdPhone`
+    flex-direction: row;
+    gap: 0.5rem;
+    `}
 `;
 
 const SelectMode = styled.div`
@@ -60,14 +103,15 @@ const SelectMode = styled.div`
   margin-bottom: 2rem;
   transition: all 0.2s ease;
   width: 20rem;
-`;
 
-export const StreakMode = styled(SelectMode)`
-  &:hover {
-    border: solid;
-    cursor: pointer;
-    background-color: #fecdae;
-  }
+  ${media.standardPhone`
+    width: 15rem;
+    margin-bottom: 1rem;
+    `}
+
+  ${media.hdPhone`
+      width: 20rem;
+      margin-bottom: 2rem;`}
 `;
 
 export const ScoreMode = styled(SelectMode)`
@@ -78,21 +122,50 @@ export const ScoreMode = styled(SelectMode)`
   }
 `;
 
+export const StreakMode = styled(SelectMode)`
+  &:hover {
+    border: solid;
+    cursor: pointer;
+    background-color: #fecdae;
+  }
+`;
+
 export const ModeTitle = styled.div`
   font-size: 2rem;
   font-weight: 800;
+
+  ${media.standardPhone`
+    font-size: 1.5rem;
+    `}
+  ${media.hdPhone`
+      font-size: 2rem;`}
 `;
 
 export const ModeIcon = styled.div`
   font-size: 7rem;
+  ${media.standardPhone`
+    font-size: 5rem;
+    `}
+  ${media.hdPhone`
+    font-size: 7rem;
+    `}
 `;
 
 export const ModeDesc = styled.span`
   font-size: 1.2rem;
+  text-align: center;
+
+  ${media.standardPhone`
+    font-size: 1rem;
+    `}
+
+  ${media.hdPhone`
+      font-size: 1.2rem;
+      `}
 `;
 
 export const DiffBtnContainer = styled.div`
   display: flex;
   gap: 0.5rem;
   padding-bottom: 1.5rem;
-`
+`;
